21cc4070435d468f14666cff047a41292aceb239
Aaron Boakye <cheminko@hotmail.com>
2026-01-30 00:09:29 +0000

    global error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |e|
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { error: "Record not found" }, status: :not_found
+  end
 end
