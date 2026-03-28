12d1e658fbc9e73329039dd78220fcdc32ca8dc8
Aaron Boakye <cheminko@hotmail.com>
2026-03-28 02:07:43 +0000

    refresh token rotation added

---
+++ db/migrate/20260328005155_create_refresh_tokens.rb
@@ -0,0 +1,12 @@
+class CreateRefreshTokens < ActiveRecord::Migration[7.1]
+  def change
+    create_table :refresh_tokens do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :token_digest
+      t.datetime :expires_at
+      t.boolean :revoked
+
+      t.timestamps
+    end
+  end
+end
